9a14878f240564b141c697922a7ce35ea8f4e67e
samgjohn23 <70330076+samgjohn23@users.noreply.github.com>
2020-09-29 15:38:17 -0400

    COMPLETES

--- Brick/Brick.gd
+++ Brick/Brick.gd
@@ -10,16 +10,30 @@ func _ready():
 
 func update_color():
 	if HUD.color_blocks:
-		pass
+		if row == 0:
+			$Color.color = Color8(224,49,49)
+		elif row == 1:
+			$Color.color = Color8(253,126,20)
+		elif row == 2:
+			$Color.color = Color8(255,224,102)
+		elif row == 3:
+			$Color.color = Color8(148,216,45)
+		elif row == 4:
+			$Color.color = Color8(34,139,230)
+		elif row == 5:
+			$Color.color = Color8(132,94,247)
+		elif row == 6:
+			$Color.color = Color8(190,75,219)
+	else:
+		$Color.color = Color(253,126,20)
+
+func emit_particle(pos):
+	if HUD.particle_paddle:
+		$Particles2D.global_position = pos
+		$Particles2D.emitting = true 
 	else:
 		pass
 
-func emit_particle(pos):
-	if HUD.particle_blocks:
-		pass
-	else:
-		pass
-	
 	
 func _on_HUD_changed():
 	update_color()
